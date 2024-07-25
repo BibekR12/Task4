485e9efc2e0d12cc398bd1426fa1d0f010081660
BibekR12 <bibekwtzjob@gmail.com>
2024-07-25 15:58:46 -0400

    create the e-com landingPg

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <nav classname="nav">
+          <ul className="ul">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Products</a>
+            </li>
+            <li>
+              <a href="#">Cart</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Item.jsx
@@ -0,0 +1,28 @@
+import "./item.css";
+function Item() {
+  return (
+    <>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/G/01/DiscoTec/2024/BTSOTC/GW/CatCard/BTSOTC24_2280_Gateway_Desktop_Category_Card_10_Tech_Under50_758x608_v3_2x._SY608_CB568491812_.jpg"
+        alt="Headphone"
+        width="200"
+        height="200"
+      ></img>
+      <img
+        src="https://m.media-amazon.com/images/I/71vk1UJniHL._AC_SY400_.jpg"
+        alt="Screen protector"
+        width="200"
+        height="200"
+      ></img>
+
+      <img
+        src="https://m.media-amazon.com/images/I/51SxIyLWwUL._AC_SY400_.jpg"
+        alt="Screen protector"
+        width="100"
+        height="200"
+      ></img>
+    </>
+  );
+}
+
+export default Item;
